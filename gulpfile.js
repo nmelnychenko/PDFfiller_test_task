9e3ed4519eca298763cbc3402d25e6d8a5d6c663
nmelnychenko <mykola.melnichenko@gmail.com>
2019-03-15 15:18:28 +0200

    sass task works right

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,36 @@
 'use strict';
 
 const gulp = require('gulp')
-const gulpLoadPlugins = require('gulp-load-plugins')
-const browserSync = require('browser-sync')
+// const gulpLoadPlugins = require('gulp-load-plugins')
+// const browserSync = require('browser-sync')
 const autoprefixer = require('gulp-autoprefixer')
 const sourceMaps = require('gulp-sourcemaps')
-const modernizer = require('gulp-modernizer')
-const uglify = require('gulp-uglify')
+// const modernizer = require('gulp-modernizer')
+// const uglify = require('gulp-uglify')
 const sass = require('gulp-sass')
 const rename = require('gulp-rename')
-const minifyCss = require('gulp-csso')
+let cleanCSS = require('gulp-clean-css')
 const concat = require('gulp-concat')
+const gulpStylelint = require('gulp-stylelint')
 
 gulp.task('sass', function () {
   return gulp.src('dev/**/*.scss')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }))
     .pipe(sass().on('error', sass.logError))
     .pipe(sourceMaps.init())
-    .pipe(autoprefixer())
+    .pipe(autoprefixer({
+			browsers: ['ie 11', 'last 2 versions'],
+			cascade: false
+		}))
     .pipe(concat('style.css'))
     .pipe(sourceMaps.write('.'))
-    .pipe(minifyCss())
+    .pipe(cleanCSS())
     .pipe(rename({
         extname: '.min.css'
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('public'))
 });
