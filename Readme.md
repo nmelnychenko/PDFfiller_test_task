15ff13ced7b1126fb856c9bedb085d60a2683459
nmelnychenko <mykola.melnichenko@gmail.com>
2019-03-17 01:15:24 +0200

    create document-list

--- Readme.md
+++ Readme.md
@@ -1,4 +1,8 @@
-To start project you must firstly install node.js and then install globally next packages:
+To start project you must firstly install **node.js** and then install globally next packages:
   - gulp-cli (`npm install gulp-cli -g`)
-  - pug-cli (`npm install pug-cli -g`)
+
+Next step: You need to install dependencies from package.json (`npm i`)
+
+Everything is prepared. To start work just run in your CLI `npm start` and open your default browser.
+If browser not opened automatically, just open in browser `http://localhost:3000/`
 
--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,33 @@
 
 const gulp = require('gulp')
 // const gulpLoadPlugins = require('gulp-load-plugins')
-// const browserSync = require('browser-sync')
+const browserSync = require('browser-sync').create()
 const autoprefixer = require('gulp-autoprefixer')
 const sourceMaps = require('gulp-sourcemaps')
 // const modernizer = require('gulp-modernizer')
 // const uglify = require('gulp-uglify')
+const plumber = require('gulp-plumber')
 const sass = require('gulp-sass')
 const rename = require('gulp-rename')
-let cleanCSS = require('gulp-clean-css')
+const cleanCSS = require('gulp-clean-css')
 const concat = require('gulp-concat')
 const gulpStylelint = require('gulp-stylelint')
+const pug = require('gulp-pug')
+const runseq = require('run-sequence')
+const clean = require('gulp-clean')
+
+gulp.task('pug', function() {
+  return gulp.src('dev/pages/*.jade')
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('public'))
+})
 
 gulp.task('sass', function () {
-  return gulp.src('dev/**/*.scss')
+  return gulp.src('dev/sass/**/*.scss')
+    .pipe(plumber())
     .pipe(gulpStylelint({
       reporters: [
         {formatter: 'string', console: true}
@@ -32,5 +46,56 @@ gulp.task('sass', function () {
     .pipe(rename({
         extname: '.min.css'
     }))
-    .pipe(gulp.dest('public'))
-});
+    .pipe(gulp.dest('public/css'))
+})
+
+
+gulp.task('clean', function () {
+  return gulp.src('public', {read: false})
+    .pipe(clean())
+})
+
+gulp.task('build', function() {
+  runseq(
+    'clean',
+    'pug',
+    'sass'
+  )
+})
+
+gulp.task('watchHtml', function () {
+  gulp.watch('dev/**/*.jade', ['pug'])
+})
+
+gulp.task('watchCss', function(){
+  gulp.watch('dev/sass/**/*.scss', ['sass'])
+})
+
+gulp.task('watch', function() {
+  runseq(
+    'watchHtml',
+    'watchCss'
+  )
+})
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+        baseDir: "public/",
+    },
+    notify: false,
+    files: [
+      'public/*.html',
+      'public/css/*.css'
+    ]
+  })
+})
+
+
+gulp.task('default', function() {
+  runseq(
+    'build',
+    'watch',
+    'server'
+  )
+})
